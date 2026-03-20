7070ee8a209edc5a515d7e724b2ac2ffb881e28e
nathaliyax <nathalialimas@hotmail.com>
2026-03-19 23:43:39 -0300

    feat: changed controller

--- src/main/java/com/projeto/ecommerce/controllers/OrderController.java
+++ src/main/java/com/projeto/ecommerce/controllers/OrderController.java
@@ -1,19 +1,29 @@
 package com.projeto.ecommerce.controllers;
 
+import com.projeto.ecommerce.DTOs.OrderDTO;
 import com.projeto.ecommerce.services.OrderService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/orders")
 public class OrderController {
 
     private final OrderService service;
 
-    public OrderController(OrderService service){
+    public OrderController(OrderService service) {
         this.service = service;
     }
 
     @PostMapping
-    public ResponseEntity
+    public ResponseEntity<OrderDTO> createOrder(@RequestBody OrderDTO dto) {
+
+        OrderDTO newOrder = service.create(dto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(newOrder);
+    }
 }
