5502db289a2c792ee5b0dd288b9a041b88a6fd6a
nathaliyax <nathalialimas@hotmail.com>
2026-04-10 11:16:13 -0300

    feat: added user request

---
+++ src/main/java/com/projeto/ecommerce/DTOs/UserRquestDTO.java
@@ -0,0 +1,12 @@
+package com.projeto.ecommerce.DTOs;
+
+
+import lombok.Data;
+
+@Data
+public class UserRquestDTO {
+
+    private String nome;
+    private String email;
+    private String senha;
+}
