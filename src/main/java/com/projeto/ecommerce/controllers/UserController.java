557942754bce673f5495842fa668b26f3136c13d
nathaliyax <nathalialimas@hotmail.com>
2026-04-10 11:12:02 -0300

    feat: added user controller

--- src/main/java/com/projeto/ecommerce/controllers/UserController.java
+++ src/main/java/com/projeto/ecommerce/controllers/UserController.java
@@ -1,4 +1,28 @@
-package com.projeto.ecommerce.controllers;
+package com.example.security.controller;
 
-public class UserController {
+import com.example.security.DTO.UsuarioRequestDTO;
+import com.example.security.service.UsuarioService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UsuarioController {
+
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity criarUsuario(@RequestBody UsuarioRequestDTO dto){
+        return ResponseEntity.ok(usuarioService.criarUsuario(dto));
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "Acesso ADMIN";
+    }
 }
