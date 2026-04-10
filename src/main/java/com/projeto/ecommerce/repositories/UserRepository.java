7745b28d103f2bca7cfdc72f1f5b2b2554c530e6
nathaliyax <nathalialimas@hotmail.com>
2026-04-10 11:12:27 -0300

    feat: added verification method

--- src/main/java/com/projeto/ecommerce/repositories/UserRepository.java
+++ src/main/java/com/projeto/ecommerce/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.projeto.ecommerce.repositories;
 
 import com.projeto.ecommerce.entities.UserEntity;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,5 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    User findByEmail(String email);
 }
